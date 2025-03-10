ab492a4ab26196d0c4fc4fbc04b9eef2460a6852
yashvi-jasoliya <yashvijasoliya42@gmail.com>
2025-03-10 12:23:33 +0530

    Vite

---
+++ basic_project_vite/src/App.jsx
@@ -0,0 +1,19 @@
+
+// import './App.css'
+import Practice from "./Practice"
+
+function App() {
+    const username = "REACT START"
+  
+
+  return (
+		// <h1>Helllo world with vite</h1>
+		<>
+			<Practice />
+            <h1>Heading tag of {username} </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore modi iusto quisquam eos iste non?</p>
+		</>
+  );
+}
+
+export default App
---
+++ basic_project_vite/src/Practice.jsx
@@ -0,0 +1,9 @@
+function Practice(){
+
+    return(
+        <h3>Hello from practice</h3>
+    )
+}
+
+
+export default Practice
