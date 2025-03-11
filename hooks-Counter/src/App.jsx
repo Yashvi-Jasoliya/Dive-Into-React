0b1c907d5a8008141525bf2386aff697049be51a
yashvi-jasoliya <yashvijasoliya42@gmail.com>
2025-03-11 10:03:59 +0530

    Hooks

---
+++ hooks-Counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	let [counter, setCounter] = useState(10);
+
+	if (counter < 0) {
+		counter = 0;
+		setCounter(counter);
+	}
+
+	if (counter > 30) {
+		counter = 30;
+		setCounter(counter);
+	}
+
+	// let counter = 1
+
+	const addvalue = () => {
+		console.log("Clicked", counter);
+		counter = counter + 1;
+		setCounter(counter);
+	};
+
+	const decreasevalue = () => {
+		console.log("Clicked", counter);
+		counter = counter - 1;
+		setCounter(counter);
+	};
+
+	return (
+		<>
+			{/* <h1>Hello from hooks</h1> */}
+			<h1>Add count between 0 to 30 </h1>
+			<h2>Counter value : {counter} </h2>
+			<button onClick={addvalue}>Add value {counter} </button>
+			<button onClick={decreasevalue}>Decrease value {counter} </button>
+		</>
+	);
+}
+
+export default App;
